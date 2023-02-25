8f0192ceeff0b26b841ed9caf007e46e27e23077
gubstav <guso22ab@student.cbs.dk>
2023-02-25 13:52:35 +0100

    changed console.log for app

--- server.js
+++ server.js
@@ -9,6 +9,5 @@ app.use('/api', require('./api/routes'));
 app.get('/', (req, res) => res.render('index'));
 
 app.listen(port, () =>
-    console.log(`Example app listening on port ${port}!`)
+    console.log(`App listening on port ${port}!`)
 );
-
